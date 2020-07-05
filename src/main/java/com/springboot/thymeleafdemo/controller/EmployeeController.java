cd8f591a3d68e60f47b22e72256a4c1fb4818399
Mateonidas <teus.polec@gmail.com>
2020-07-05 13:42:35 +0200

    Added Get Employees.

--- src/main/java/com/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,36 +1,28 @@
 package com.springboot.thymeleafdemo.controller;
 
-import com.springboot.thymeleafdemo.model.Employee;
+import com.springboot.thymeleafdemo.entity.Employee;
+import com.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    private List<Employee> employees;
+    private EmployeeService employeeService;
 
-    @PostConstruct
-    private void loadData(){
-        Employee employee1 = new Employee(1, "Leslie", "Andrews", "lesli@test.com");
-        Employee employee2 = new Employee(1, "Emma", "Bumgat", "emma@test.com");
-        Employee employee3 = new Employee(1, "Avani", "Gupta", "avani@test.com");
-
-        employees = new ArrayList<>();
-
-        employees.add(employee1);
-        employees.add(employee2);
-        employees.add(employee3);
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/list")
     public String listEmployees(Model model){
+
+        List<Employee> employees = employeeService.findAll();
         model.addAttribute("employees", employees);
 
         return "list-employees";
--- src/main/java/com/springboot/thymeleafdemo/entity/Employee.java
+++ src/main/java/com/springboot/thymeleafdemo/entity/Employee.java
@@ -1,13 +1,32 @@
-package com.springboot.thymeleafdemo.model;
+package com.springboot.thymeleafdemo.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email")
     private String email;
 
     public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
     }
 
     public Employee(int id, String firstName, String lastName, String email) {
@@ -47,5 +66,15 @@ public class Employee {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
