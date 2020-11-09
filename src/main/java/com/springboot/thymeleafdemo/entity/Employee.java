b6c607e446e7067936fd7d193450bf6af513a3c5
Mateonidas <teus.polec@gmail.com>
2020-11-09 18:48:26 +0100

    Init.

--- src/main/java/com/springboot/thymeleafdemo/entity/Employee.java
+++ src/main/java/com/springboot/thymeleafdemo/entity/Employee.java
@@ -1,7 +1,12 @@
 package com.springboot.thymeleafdemo.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "employee")
 public class Employee {
@@ -11,70 +16,28 @@ public class Employee {
     @Column(name = "id")
     private int id;
 
-    @Column(name = "first_name")
-    private String firstName;
-
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "email")
-    private String email;
+    @Column(name = "salary")
+    private Double salary;
+
+    @Column(name = "firm")
+    private String firm;
 
     public Employee() {
     }
 
-    public Employee(String firstName, String lastName, String email) {
-        this.firstName = firstName;
+    public Employee(String lastName, Double salary, String firm) {
         this.lastName = lastName;
-        this.email = email;
+        this.salary = salary;
+        this.firm = firm;
     }
 
-    public Employee(int id, String firstName, String lastName, String email) {
+    public Employee(int id, String lastName, Double salary, String firm) {
         this.id = id;
-        this.firstName = firstName;
         this.lastName = lastName;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                '}';
+        this.salary = salary;
+        this.firm = firm;
     }
 }
